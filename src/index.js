8b539148635044443cc065898d51fc623855cfb7
Sam Domesjö <samdomesjo@gmail.com>
2018-02-09 23:02:11 +0000

    adding nav

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import './styles/styles.scss';
 
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Grid, Row } from 'react-bootstrap';
+
+import Nav from './nav'
 
 class Portfolio extends Component {
     render() {
         return(
-            <div>
-                Under construction
-            </div>
+            <Grid>
+                <Nav />
+                <Row>
+                    Under construction
+                </Row>
+            </Grid>
         )
     }
 }
